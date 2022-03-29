5b0a517ac8f504c7cb2a38a02b83d13cd77056e2
hamzaj1127 <89408188+hamzaj1127@users.noreply.github.com>
2022-03-29 11:42:37 -0400

    Add files via upload

---
+++ Bank/BANK.py
@@ -0,0 +1,17 @@
+customer_name = input('what is your name?')
+starting_balance = (float(5000.25))
+print('Hello',customer_name, 'your balance is', starting_balance)
+pay_check = float(input('how much would you like to deposit?'))
+expenditure_item = input('What did you buy?')
+expenditure = float(input('How much did it cost?'))
+def checking_balance ( user_name, balance,deposits, expense_item, expense_amount):
+    user_name
+    balance
+    deposits
+    expense_item
+    expense_amount
+ending_balance = (starting_balance ++ pay_check -- expenditure)
+
+print('your final balance is ',ending_balance)
+
+
